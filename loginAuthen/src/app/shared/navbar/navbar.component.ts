ccd3cfd83fefb76ffbad38f40f5c51bc2bccc1b3
Xioamaaa <143615414+Xioamaaa@users.noreply.github.com>
2025-04-14 02:18:43 -0400

    mgame

--- loginAuthen/src/app/shared/navbar/navbar.component.ts
+++ loginAuthen/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
